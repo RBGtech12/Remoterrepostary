4b1546c714fcab9380e3b73915db510feebd0387
YallareddyMaddirala <maddiralayallareddy293@gmail.com>
2023-08-27 11:39:05 +0530

    msg

---
+++ ArrayDemo6.java
@@ -0,0 +1,27 @@
+package Javademos;
+
+public class ArrayDemo6 {
+
+	public static void main(String[] args) 
+	{
+		 
+		ArrayDemo6 a1 = new ArrayDemo6();
+       
+       
+		int k[]=a1.funA();
+		
+		System.out.println(k[2]);
+		
+		System.out.println(a1.funA()[2]);
+
+	}
+     
+	
+	int[] funA()
+	{
+		
+		int k[] = {10, 20,30,40,50};
+		
+		return k;
+	}
+}
